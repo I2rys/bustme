f6ad867128e6f8f5b6c69f4d1294d2a19bae59e1
I2rys <i2rys@protonmail.com>
2021-12-28 07:47:40 +0800

    BustMe

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center">BustMe</h1>
+<h4 align="center">Blazing fast & one of the best simple website content discover</h4>
+<p align="center">
+	<a href="https://github.com/I2rys/BustMe/blob/main/LICENSE"><img src="https://img.shields.io/github/license/I2rys/BustMe?style=flat-square"></img></a>
+	<a href="https://github.com/I2rys/BustMe/issues"><img src="https://img.shields.io/github/issues/I2rys/BustMe.svg"></img></a>
+	<a href="https://nodejs.org/"><img src="https://img.shields.io/badge/-Nodejs-green?style=flat-square&logo=Node.js"></img></a>
+</p>
+
+
+## Installation
+Github:
+
+    git clone https://github.com/I2rys/BustMe
+    
+NPM Packages:
+```
+npm i axios && npm i delay
+```
+
+## Usage
+```
+node index.js <url> <dictionary> <output>
+```
+
++ url - The target website url.
++ dictionary - Your path's dictionary.
++ output - In where to save the valid path's found.
+
+## License
+MIT © I2rys
---
+++ index.js
@@ -0,0 +1,80 @@
+//Dependencies
+const Axios = require("Axios")
+const Delay = require("delay")
+const Fs = require("fs")
+
+//Variables
+const Self_Args = process.argv.slice(2)
+
+var Self = {
+    max: 0,
+    gh: false,
+    valid: []
+}
+
+//Functions
+async function check(dictionary, i){
+    await Delay(1000)
+
+    if(!dictionary[i]){
+        return Self.max++
+    }
+
+    try{
+        const response = await Axios({
+            method: "GET",
+            url: `${Self_Args[0]}/${dictionary[i]}`
+        })
+        
+        if(response.status === 200 || response.status === 201 || response.status === 202 || response.status === 203 || response.status === 204){
+            console.log(`${Self_Args[0]}/${dictionary[i]} - Got status 200/201/202/203/204`)
+            Self.valid.push(`${Self_Args[0]}/${dictionary[i]}`)
+            Self.gh = true
+        }
+
+        Self.max++
+    }catch{
+        Self.max++
+    }
+
+    if(Self.max === dictionary.length){
+        if(!Self.gh){
+            console.log("No valid path found.")
+        }
+
+        console.log(`Saving the results to ${Self_Args[2]}`)
+        Fs.writeFileSync(Self_Args[2], Self.valid.join("\n"), "utf8")
+        console.log(`Results successfully saved to ${Self_Args[2]}`)
+
+        console.log("Finished checking.")
+        process.exit()
+    }
+}
+
+//Main
+if(!Self_Args.length){
+    console.log("node index.js <url> <dictionary> <output>")
+    process.exit()
+}
+
+if(!Fs.existsSync(Self_Args[1])){
+    console.log("Invalid dictionary")
+    process.exit()
+}
+
+if(!Self_Args[2]){
+    console.log("Invalid output.")
+    process.exit()
+}
+
+const dictionary_data = Fs.readFileSync(Self_Args[1], "utf8").replace(/\r/g, "").split("\n")
+
+if(!dictionary_data.length){
+    console.log("Dictionary data is empty.")
+    process.exit()
+}
+
+console.log("Scanning the website, please wait.")
+for( i = 0; i <= dictionary_data.length-1; i++ ){
+    check(dictionary_data, i)
+}
